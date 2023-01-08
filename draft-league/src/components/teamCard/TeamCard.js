558308cd44cfd54dc606b12679fba4e7b3bd8d47
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2023-01-08 14:41:58 -0800

    import bootstrap

--- draft-league/src/components/teamCard/TeamCard.js
+++ draft-league/src/components/teamCard/TeamCard.js
@@ -1,9 +1,4 @@
 export function TeamCard(input) {
-  let amountOfTeams = Number(input.input);
-  console.log(typeof amountOfTeams);
-  let teams = [...Array(amountOfTeams).keys()].map((x) => ++x);
-  console.log(teams);
-
   return (
     <>
       <h2>Team</h2>
