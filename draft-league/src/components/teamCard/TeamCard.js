1ff703e5cb365cb218c64a7eea7f84eeb846f316
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2023-01-08 16:55:44 -0800

    update teamcard

--- draft-league/src/components/teamCard/TeamCard.js
+++ draft-league/src/components/teamCard/TeamCard.js
@@ -1,12 +1,1 @@
-export function TeamCard(input) {
-  return (
-    <>
-      <h2>Team</h2>
-      <form>
-        <input type="text"></input>
-        <button type="submit">Add</button>
-      </form>
-      <ol></ol>
-    </>
-  );
-}
+
